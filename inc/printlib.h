a28f5c851d01c9cd6139467188735652e0078de4
Luke Ruatta <luke.ruatta@friedrich.com>
2024-12-27 13:46:49 -0600

    printlib managed by CPM now, pulls dev on cmake configuration and all we have to change is the GIT_TAG property to match the branch name

--- inc/printlib.h
+++
@@ -1,8 +0,0 @@
-
-
-#ifndef LIB_PRINTLIB_H_
-#define LIB_PRINTLIB_H_
-
-void printHelloWorld(void);
-
-#endif /* LIB_PRINTLIB_H_ */
--- src/cmake_basic.cpp
+++ src/cmake_basic.cpp
@@ -1,13 +1,21 @@
-//#include <iostream>
+#include <iostream>
 using namespace std;
 
 extern "C" 
 {
 #include "printlib.h"
+//#include "sw_version.h"
+}
+
+void printVersionInfo(void)
+{
+    //std::cout << project_version;
 }
 
 int main(int argc, char **argv) 
 {
+    printVersionInfo();
+
     printHelloWorld();
 
 	return 0;
